3509dce7129f18f9c7c021e0f86eb061bed2175d
gfw-breaker <lixiaoming0513@gmail.com>
2022-04-17 21:09:13 +0800

    ok

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ version="1.3.1"
 
 wget https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
-cd node_exporter-*.*-amd64
-cp node_exporter /usr/local/bin/
+cp node_exporter-*.*-amd64/node_exporter /usr/local/bin/
 
 useradd node_exporter --shell=/sbin/nologin
 
