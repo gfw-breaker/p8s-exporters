73e76c004d7a3d37d77d1ab23c0e0e589ff7f3c2
gfw-breaker <lixiaoming0513@gmail.com>
2022-04-23 17:38:53 +0800

    ok

--- install_nginx_exporter.sh
+++ install_nginx_exporter.sh
@@ -4,7 +4,7 @@ version="0.10.0"
 
 #wget https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz
 wget https://github.com/nginxinc/nginx-prometheus-exporter/releases/download/v${version}/nginx-prometheus-exporter_${version}_linux_amd64.tar.gz 
-tar xvfz nginx-prometheus-exporter.*amd64.tar.gz
+tar xvfz nginx-prometheus-exporter*amd64.tar.gz
 cp nginx-prometheus-exporter /usr/local/bin/nginx_exporter
 
 useradd nginx_exporter --shell=/sbin/nologin
