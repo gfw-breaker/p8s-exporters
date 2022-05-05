096e303a0293dadb3f6feba26bf2b50d4531628d
gfw-breaker <lixiaoming0513@gmail.com>
2022-05-05 22:51:32 +0800

    ok

---
+++ install_v2ray_exporter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#url=https://github.com/wi1dcard/v2ray-exporter/releases/latest/download/v2ray-exporter_linux_amd64
+#wget $url -O /tmp/v2ray-exporter
+#mv /tmp/v2ray-exporter /usr/local/bin/v2ray-exporter
+
+cp binary/v2ray-* /usr/local/bin/v2ray-exporter
+chmod +x /usr/local/bin/v2ray-exporter
+
+useradd v2ray_exporter --shell=/sbin/nologin
+
+cp v2ray_exporter.service /etc/systemd/system/
+systemctl enable v2ray_exporter --now
+
+
